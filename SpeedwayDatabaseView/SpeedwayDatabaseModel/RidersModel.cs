0e1799cc85f82a17739965ebad57ebb1aa01e773
kingkong1946 <ckmkingkong@gmail.com>
2015-08-11 14:26:00 +0200

    Some changes in CenExecuteChanged.

---
+++ SpeedwayDatabaseView/SpeedwayDatabaseModel/RidersModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SpeedwayDatabaseModel
+{
+    public class RidersModel : ICommand
+    {
+        #region Fields
+        private readonly Predicate<object> _canExecute;
+        private readonly Action<object> _execute;
+        #endregion
+
+        #region Constructors
+        public RidersModel(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null) throw new ArgumentNullException("execute is NULL");
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public RidersModel(Action<object> execute) : this(execute, null)
+        {
+        }
+        #endregion
+
+        #region ICommand Members
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        #endregion
+    }
+}
